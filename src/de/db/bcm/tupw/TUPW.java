39e42e87f0b396061595937f49ceb7caec7ea324
Frank Schwab <frank.schwab@deutschebahn.com>
2020-03-27 11:12:09 +0100

    Adapted command line example to new structure.

--- src/de/db/bcm/tupw/TUPW.java
+++ src/de/db/bcm/tupw/TUPW.java
@@ -2,19 +2,20 @@
  * Copyright (c) 2020, DB Systel GmbH
  * All rights reserved.
  *
- * SPDX-License-Identifier: Apache-2.0
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * Author: Frank Schwab, DB Systel GmbH
  *
@@ -39,11 +40,12 @@
  *     2019-03-07: V4.0.1: Strengthen test for invalid blinding data. fhs
  *     2019-03-07: V4.1.0: Correct handling of keys with subject parameter. fhs
  *     2020-03-06: V4.1.1: Simply read from System.in. fhs
+ *     2020-03-27: V4.1.2: Adapted to new interface.. fhs
  */
-package TUPW;
+package de.db.bcm.tupw;
 
-import dbscryptolib.FileAndKeyEncryption;
-import dbsnumberlib.Xoroshiro128plusplus;
+import de.db.bcm.tupw.crypto.FileAndKeyEncryption;
+import de.db.bcm.tupw.numbers.Xoroshiro128plusplus;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -60,7 +62,7 @@ import java.io.IOException;
  * </p>
  *
  * @author Frank Schwab, DB Systel GmbH
- * @version 4.1.1
+ * @version 4.1.2
  */
 public class TUPW {
    private static final int MAX_INPUT_BYTES = 50000000;
@@ -116,7 +118,7 @@ public class TUPW {
             if (args[0].substring(0, 1).toLowerCase().equals("e")) {
                System.out.println(myEncryptor.encryptData(getInputFromWhereEver(args[itemIndex]), subject));
             } else {
-               System.out.println(myEncryptor.decryptData(getInputFromWhereEver(args[itemIndex]), subject));
+               System.out.println(myEncryptor.decryptDataAsString(getInputFromWhereEver(args[itemIndex]), subject));
             }
          } catch (final Exception e) {
             e.printStackTrace();
